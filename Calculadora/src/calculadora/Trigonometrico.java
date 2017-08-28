67cf5def017f482fb53f9a8555d451a3b90d3f84
Estudiantes <Estudiantes@Labing70208.udistrital.edu.co>
2017-08-28 14:45:43 -0500

    Pasando de radianes a grados

--- Calculadora/src/calculadora/Trigonometrico.java
+++ Calculadora/src/calculadora/Trigonometrico.java
@@ -1,14 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package calculadora;
 
-/**
- *
- * @author Estudiantes
- */
-public class Trigonometrico {
+
+public class Trigonometrico extends Calculadora {
+    void transformar(){
+        operando1=(float)
+     
     
+}
+    
+    void seno(){
+        transformar()
+        resultado=((float)Math.sin(operando1));
+    }
+    void coseno(){
+        resultado=((float)Math.cos(operando1));
+    }    
+    void tangente(){
+        resultado=((float)Math.tan(operando1));
+    }
 }
