50b2be3b70a6ba762259e3702987945ce5092f09
Estudiantes <Estudiantes@Labing70208.udistrital.edu.co>
2017-08-28 14:52:52 -0500

    Final Final Eliana Rodriguez MIguel Rodriguez

--- Calculadora/src/calculadora/Trigonometrico.java
+++ Calculadora/src/calculadora/Trigonometrico.java
@@ -4,19 +4,21 @@ package calculadora;
 
 public class Trigonometrico extends Calculadora {
     void transformar(){
-        operando1=(float)
+        operando1=(float)Math.toRadians(operando1);
      
     
 }
     
     void seno(){
-        transformar()
+        transformar();
         resultado=((float)Math.sin(operando1));
     }
     void coseno(){
+        transformar();
         resultado=((float)Math.cos(operando1));
     }    
     void tangente(){
+        transformar ();
         resultado=((float)Math.tan(operando1));
     }
 }
